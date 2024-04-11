a634fd98488d4bbb7c4b0082e009505f2bf8e649
Janek <janek.labeda@gmail.com>
2024-04-11 18:38:12 +0200

    - Removed unused file - Refresh token interceptor

--- lib/core/dio_client.dart
+++ lib/core/dio_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jlabeda_uniqcast_task/auth/controllers/auth_notifier_provider.dart';
 import 'package:jlabeda_uniqcast_task/auth/controllers/auth_state.dart';
@@ -26,9 +27,41 @@ class DioClient {
         'Authorization': 'Bearer ${_authState.authData?.accesToken}',
       },
     );
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onError: (error, handler) async {
+          debugPrint(
+            'Dio Interceptor: Old token - ${_authState.authData?.accesToken}',
+          );
+          if (error.response?.statusCode == 401) {
+            final newToken = await _refreshToken();
+            debugPrint('[Dio Interceptor: New token - $newToken]');
+            error.requestOptions.headers['Authorization'] = 'Bearer $newToken';
+            return handler.resolve(await dio.fetch(error.requestOptions));
+          }
+          return handler.next(error);
+        },
+      ),
+    );
   }
 
   final Dio _dio = Dio();
   final AuthState _authState;
   Dio get dio => _dio;
+
+  Future<String> _refreshToken() async {
+    try {
+      final response = await dio.post<Map<String, dynamic>>(
+        'v2/global/refresh',
+        data: {
+          'refresh_token': _authState.authData?.refreshToken,
+        },
+      );
+      return (response.data?['data'] as Map<String, dynamic>)['access_token']
+          as String;
+    } catch (e) {
+      debugPrint('DioClient refreshToken() exception: $e');
+      return _authState.authData?.accesToken ?? '';
+    }
+  }
 }
